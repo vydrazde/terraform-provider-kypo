e62ec1bdb6aa9b99a2877ef948494e256ec89ef3
Zdeněk Vydra <xvydra@fi.muni.cz>
2023-03-16 12:52:51 +0100

    Fix example

--- examples/resources/scaffolding_example/resource.tf
+++ examples/resources/scaffolding_example/resource.tf
@@ -1,4 +1,4 @@
-resource "kypo_definitions" "example" {
+resource "kypo_sandbox_definition" "example" {
   url = "git@gitlab.ics.muni.cz:muni-kypo-trainings/games/junior-hacker.git"
   rev = "master"
 }

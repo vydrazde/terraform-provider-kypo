0aec4649d5eb59d48557e52422d43bd7c72932b1
Zdeněk Vydra <xvydra@fi.muni.cz>
2023-04-13 14:13:08 +0200

    Use general testing definition for sandbox pool acc test

--- internal/provider/sandbox_pool_resource_test.go
+++ internal/provider/sandbox_pool_resource_test.go
@@ -66,7 +66,7 @@ resource "kypo_sandbox_pool" "test" {
 			{
 				Config: providerConfig + `
 resource "kypo_sandbox_definition" "test" {
-  url = "git@gitlab.ics.muni.cz:muni-kypo-trainings/games/junior-hacker.git"
+  url = "git@gitlab.ics.muni.cz:muni-kypo-crp/prototypes-and-examples/sandbox-definitions/general-testing-definition.git"
   rev = "master"
 }
 resource "kypo_sandbox_pool" "test" {
